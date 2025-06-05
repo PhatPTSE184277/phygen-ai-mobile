84b7f7dbef1ff03a4a8b9b61f6cd1b38804b9811
Phạm Tấn Phát <tanphat120104@gmail.com>
2025-06-05 09:30:58 +0700

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import './global.css';
+import { Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View className="flex-1 justify-center items-center bg-white">
+            <Text className="text-lg font-bold">Na</Text>
+        </View>
+    );
+}
